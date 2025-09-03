c451fdb72fa89d4a2cc56ab9e834a0f6d02f77ea
mills32 <biosystemxy@gmail.com>
2025-09-03 14:14:54 +0200

    Add files via upload

--- opengl_make.bat
+++ opengl_make.bat
@@ -1,2 +1,6 @@
-gcc -std=c11 opengl.c -Wall -lopengl32 -lglu32 -lglut32 -lcomdlg32 -o opengl
+ld -r -b binary -o gui0.data.o gui0.data
+ld -r -b binary -o numbers.data.o numbers.data
+gcc -std=c11 opengl.c numbers.data.o gui0.data.o -Wall -lopengl32 -lglu32 -lglut32 -lcomdlg32 -Wl,-subsystem,windows -o vgamapedit
+del numbers.data.o
+del gui0.data.o
 pause
